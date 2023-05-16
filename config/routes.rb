36b0784bf97c53a22470442c3b3023e25fec6106
Maron6676 <127210557+Maron6676@users.noreply.github.com>
2023-05-16 09:14:55 +0000

    [add] devise:controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  devise_for :customer
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
