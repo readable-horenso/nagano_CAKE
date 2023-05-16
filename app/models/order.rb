52e153c0290eaeef10bf9bcfc5e599f180bff28d
sato-ayumi <129816397+sato-ayumi@users.noreply.github.com>
2023-05-16 13:54:20 +0000

    [Finish] model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,18 @@
 class Order < ApplicationRecord
+  belongs_to :customer
+  has_many :order_details
+  
+  # 支払い方法 （０= クレジットカード / １= 銀行振込）
+  enum payment_method: { 
+      credit_card: 0,
+      transfer: 1
+  }
+  # 注文ステータス （０= 入金待ち / １= 入金確認 / ２= 製作中 / ３= 発送準備中 / ４= 発送済み  ）
+  enum status: { 
+      payment_waiting: 0,
+      payment_confirmation: 1,
+      in_production: 2,
+      preparing_delivery: 3,
+      delivered: 4
+  }
 end
