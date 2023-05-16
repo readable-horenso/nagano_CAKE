f7d3292b8f4d38fa5e3e2d4aa2c1bf81d95f3892
TsuKi-3Moti <129638089+TsuKi-3Moti@users.noreply.github.com>
2023-05-16 12:38:49 +0000

    add routes.rb public

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,15 @@
 class Public::OrdersController < ApplicationController
+
   def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
   end
 
   def confirm
@@ -8,9 +18,4 @@ class Public::OrdersController < ApplicationController
   def complete
   end
 
-  def index
-  end
-
-  def show
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,31 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'destinations/index'
-    get 'destinations/edit'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/complete'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/unsubscribe'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
+
+  root to:             "homes#top"
+  get "homes/about" => "homes#about", as: "about"
+
   devise_for :admin
 
-  # 顧客用
+    # 顧客用
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
-    sessions: 'public/sessions'
+    sessions:      "public/sessions"
   }
+
+  resources :items, only: [:index, :show]
+
+  resource :customers, only: [:show, :update]
+  get    "customers/information/edit" => "customers#edit",        as: "edit"
+  get    "customers/unsubscribe"      => "customers#unsubscribe", as: "unsubscribe"
+  patch  "customers/withdraw"         => "customers#withdraw",    as: "withdraw"
+
+  resources :cart_items, only: [:index, :create, :update, :destroy]
+  delete "cart_items/destroy_all" => "cart_items#destroy_all", as: "destroy_all"
+
+  resources :orders, only: [:new, :index, :create, :show]
+  post   "orders/confirm"  => "orders#confirm",  as: "confirm"
+  get    "orders/complete" => "orders#complete", as: "complete"
+
+  resources :destinations, only: [:index, :edit, :create, :update, :destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
