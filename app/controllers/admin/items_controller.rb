21e3fbacdce93fc656ec2a69134a758d14dda496
takayuki91 <takayuki.nakatsuji@gmail.com>
2023-05-17 10:36:03 +0000

    [defect]item create

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,19 +1,34 @@
 class Admin::ItemsController < ApplicationController
+
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
   end
 
   def index
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
   end
 
-  def create
-  end
-
   def update
   end
+
+  private
+
+  def item_params
+    # 後にジャンルも許可する
+    params.require(:item).permit(:image, :name, :description, :genre_id, :price, :is_valid)
+  end
+
 end
