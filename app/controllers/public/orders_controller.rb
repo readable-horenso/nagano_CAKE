3dae558e3889f5df6227f3385ad2e73286fb2311
TsuKi-3Moti <129638089+TsuKi-3Moti@users.noreply.github.com>
2023-05-17 06:45:49 +0000

    [Fix] rotes.rb public scope module collection

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,13 +2,13 @@ class Public::OrdersController < ApplicationController
 
   def new
   end
-
+  
   def index
   end
-
+  
   def show
   end
-
+  
   def create
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,24 +9,35 @@ Rails.application.routes.draw do
   }
 
   scope module: :public do
-    root to:             "homes#top"
+
+    root to:       "homes#top"
     get "about" => "homes#about", as: "about"
 
     resources :items, only: [:index, :show]
 
-    resource :customers, only: [:show, :update]
-    get    "customers/information/edit" => "customers#edit",        as: "edit"
-    get    "customers/unsubscribe"      => "customers#unsubscribe", as: "unsubscribe"
-    patch  "customers/withdraw"         => "customers#withdraw",    as: "withdraw"
+    resource :customers, only: [:show, :update] do
+      get      "information/edit" => "customers#edit",        as: "edit"
+      collection do
+        get    "unsubscribe"      => "customers#unsubscribe", as: "unsubscribe"
+        patch  "withdraw"         => "customers#withdraw",    as: "withdraw"
+      end
+    end
 
-    resources :cart_items, only: [:index, :create, :update, :destroy]
-    delete "cart_items/destroy_all" => "cart_items#destroy_all", as: "destroy_all"
+    resources :cart_items, only: [:index, :create, :update, :destroy] do
+      collection do
+        delete "destroy_all" => "cart_items#destroy_all", as: "destroy_all"
+      end
+    end
 
-    resources :orders, only: [:new, :index, :create, :show]
-    post   "orders/confirm"  => "orders#confirm",  as: "confirm"
-    get    "orders/complete" => "orders#complete", as: "complete"
+    resources :orders, only: [:new, :index, :create, :show] do
+      collection do
+        post   "confirm"  => "orders#confirm",  as: "confirm"
+        get    "complete" => "orders#complete", as: "complete"
+      end
+    end
 
     resources :destinations, only: [:index, :edit, :create, :update, :destroy]
+
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
