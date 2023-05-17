08c45a2ad073302df2697c4660586f67f3d0b4c8
sato-ayumi <129816397+sato-ayumi@users.noreply.github.com>
2023-05-17 12:49:17 +0000

    [Add] Login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def after_sign_up_path_for(resource)
+    case resource
+    when Customer
+      customers_path
+    end
+  end
+ 
+  def after_sign_out_path_for(resource)
+    case resource
+    when :admin 
+      new_admin_session_path
+    when :customer
+      root_path
+    end
+  end
+  
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :given_name, :family_name_kana, :given_name_kana, :email, :postcode, :address, :phone_number])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create!(email: "admin@gmail.com",
+              encrypted_password: "121212",)
