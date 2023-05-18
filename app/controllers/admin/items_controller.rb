d95e30f84ee46f0397319f360868acd0602eae07
takayuki91 <takayuki.nakatsuji@gmail.com>
2023-05-18 09:06:26 +0000

    [Add]partial genre&item

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,12 +1,12 @@
 class Admin::ItemsController < ApplicationController
 
   # before_action :authenticate_admin!
-  
+
   def index
     @items = Item.all
     # .page(params[:page]).per(10)
   end
-  
+
   def new
     @item = Item.new
   end
@@ -14,7 +14,7 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if  @item.save
-        flash[:notice] = "商品が新規登録されました"
+        flash[:notice] = '商品が新規登録されました'
         redirect_to admin_items_path
     else
         render :new
@@ -32,7 +32,7 @@ class Admin::ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if  @item.update(item_params)
-        flash[:notice] = "商品が更新されました"
+        flash[:notice] = '商品が更新されました'
         redirect_to admin_item_path(@item)
     else
         render :edit
@@ -42,7 +42,6 @@ class Admin::ItemsController < ApplicationController
   private
 
   def item_params
-    # 後にジャンルも許可する
     params.require(:item).permit(:image, :name, :description, :genre_id, :price, :is_valid)
   end
 
