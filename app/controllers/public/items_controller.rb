636f8c74994620e1b20c64c4a39150f47f95596c
TsuKi-3Moti <129638089+TsuKi-3Moti@users.noreply.github.com>
2023-05-20 02:21:38 +0000

    [Add] view layouts

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,9 +1,15 @@
 class Public::ItemsController < ApplicationController
-  
+
   def index
   end
 
   def show
+    @item = Item.find(params[:id])
   end
-  
+
+  private
+
+  def params_cart_item
+    params.reqire(:cart_item).permit(:quantity)
+  end
 end
