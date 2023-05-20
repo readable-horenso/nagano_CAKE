dba578faa9839a25ecc5d9145ba09bd47afbfa8f
sato-ayumi <129816397+sato-ayumi@users.noreply.github.com>
2023-05-20 06:56:00 +0000

    [Add] layout all

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,6 +8,17 @@ class Customer < ApplicationRecord
   has_many :destinations, dependent: :destroy
   has_many :orders, dependent: :destroy
   
+  with_options presence: true do
+    validates :family_name
+    validates :given_name
+    validates :family_name_kana
+    validates :given_name_kana
+    validates :postcode
+    validates :address
+    validates :phone_number
+    validates :email
+  end
+  
   # 会員フルネーム
   def full_name
     self.family_name + "　" + self.given_name
