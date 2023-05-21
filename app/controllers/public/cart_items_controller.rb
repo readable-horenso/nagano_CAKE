23de0b8f7645df8ec3a1d6e0f2cdcf7183a931c9
TsuKi-3Moti <129638089+TsuKi-3Moti@users.noreply.github.com>
2023-05-21 07:19:04 +0000

    [Add] update destroy destroy_all

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,18 +6,26 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @cart_item = current_customer.cart_items.new(cart_item_params)
-    # @cart_item.item_id = Item.id
     @cart_item.save
     redirect_to cart_items_path
   end
 
   def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
 
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
   end
 
   def destroy_all
+    @cart_items = CartItem.where(customer_id: current_customer.id)
+    @cart_items.destroy_all
+    redirect_to cart_items_path
   end
 
   private
