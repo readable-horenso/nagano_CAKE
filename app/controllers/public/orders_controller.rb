737592a979547fb0036367b7381964981767bfd0
Maron6676 <127210557+Maron6676@users.noreply.github.com>
2023-05-22 08:05:53 +0000

    [Add]order-index.show view

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,9 +5,12 @@ class Public::OrdersController < ApplicationController
   end
 
   def index
+    @orders = current_customer.orders
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details.all
   end
 
   def create
