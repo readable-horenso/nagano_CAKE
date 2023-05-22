0262f7750c59a64e8ea3177f32a5927aa1061569
Maron6676 <127210557+Maron6676@users.noreply.github.com>
2023-05-22 06:19:29 +0000

    [Add]order-function　controller view

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -11,18 +11,18 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-    cart_items = current_customer.cart_items.all
     @order = current_customer.orders.new(order_params)
     if @order.save
-      cart_items.each do |cart|
-        order_detail = OrderDetail.new
-        order_detail.item_id = cart.item_id
-        order_detail.order_id = @order.id
-        order_detail.quantity = cart.quantity
-        order_detail.price = cart.price
+      current_customer.cart_items.each do |cart|
+        @order_detail = OrderDetail.new
+        @order_detail.item_id = cart.item_id
+        @order_detail.order_id = @order.id
+        @order_detail.quantity = cart.quantity
+        @order_detail.price = cart.item.with_tax_price
+        @order_detail.save
       end
-      redirect_to confirm_orders_path
-      cart_items.desrtoy_all
+      current_customer.cart_items.destroy_all
+      redirect_to complete_orders_path
     else
       @order = Order.new(order_params)
       render :new
@@ -31,31 +31,40 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
-    
+
     # 現在の住所の場合
     if params[:order][:address_option] == "0"
-      @order.destination_post_code = current_customer.post_code
-      @order.destination_address = current_customer.address
-      @order.destination_name = current_customer.family_name + current_customer.given_name
-      
-    #　登録した配送先の場合
+      @order.postcode = current_customer.postcode
+      @order.address = current_customer.address
+      @order.name = current_customer.family_name + current_customer.given_name
+
+    #　登録済み住所の場合
     elsif params[:order][:address_option] == "1"
-      destination = Address.find(params[:order][:customer_id])
-      @order.destination_post_code = destination.post_code
-      @order.destination_address = destination.address
-      @order.destination_name = destination.name
-      
+      destination = Destination.find(params[:order][:destination_id])
+      @order.postcode = destination.postcode
+      @order.address = destination.address
+      @order.name = destination.name
+
     #　新規住所入力の場合
-    elsif params[:order][:address_option] = "2"
-      @order.destination_post_code = params[:order][:destination_post_code]
-      @order.destination_address = params[:order][:destination_address]
-      @order.destination_name = params[:order][:destination_name]
+    elsif params[:order][:address_option] == "2"
+      @order.postcode = params[:order][:postcode]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
     else
       render "new"
     end
+
+    @cart_items = current_customer.cart_items.all
+    @order.customer_id = current_customer.id
   end
 
   def complete
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:postage, :payment_method, :name, :address, :postcode ,:customer_id ,:total_amount ,:status)
   end
 
 end
