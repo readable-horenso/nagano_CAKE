3a1577e64d64a6e93c188bd91aa1dc46d48a26c9
takayuki91 <takayuki.nakatsuji@gmail.com>
2023-05-24 08:51:39 +0000

    [Fix]ja.yml

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,28 +1,19 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details
-  
+
   # 支払い方法 （０= クレジットカード / １= 銀行振込）
-  enum payment_method: { 
+  enum payment_method: {
       credit_card: 0,
       transfer: 1
   }
   # 注文ステータス （０= 入金待ち / １= 入金確認 / ２= 製作中 / ３= 発送準備中 / ４= 発送済み  ）
-  enum status: { 
+  enum status: {
       payment_waiting: 0,
       payment_confirmation: 1,
       in_production: 2,
       preparing_delivery: 3,
       delivered: 4
   }
-  
-  def self.statuses_i18n
-  {
-    "payment_waiting" => "入金待ち",
-    "payment_confirmation" => "入金確認",
-    "in_production" => "製作中",
-    "preparing_delivery" => "発送準備中",
-    "production_complete" => "製作完了"
-  }
-  end
+
 end
