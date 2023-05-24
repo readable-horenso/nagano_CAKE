03bbe9f028a94ba397beb17d894348fd5aed7f72
sato-ayumi <129816397+sato-ayumi@users.noreply.github.com>
2023-05-24 11:40:04 +0000

    [Fix] test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,6 @@ class ApplicationController < ActionController::Base
   
   private
   
-  
   def after_sign_in_path_for(resource_or_scope)
     if resource_or_scope.is_a?(Admin)
       admin_root_path
